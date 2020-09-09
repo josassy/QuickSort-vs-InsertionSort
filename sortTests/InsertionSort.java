0d2da07daa1140d0275e4226d153ae17f2268bec
Rufus Roby Mathew <rufusrobymathew@gmail.com>
2020-09-09 07:41:11 -0400

    Completed InsertionSort

--- sortTests/InsertionSort.java
+++ sortTests/InsertionSort.java
@@ -1,8 +1,21 @@
 package sortTests;
 
 public class InsertionSort {
+	void sort (int array[])
+	{
+		int n = array.length; //getting the length of the array to be sorted
+		for(int i = 1; 1 < n; i++){ //starting the loop 
+			int key = array[i]; 
+			int j = i - 1;
 
-  public static String testSanity() {
-    return "hello from insertion sort";
-  }
+			//Moving all the elements greater than key by one increment
+
+			while (j>=0 && array[j] > key){
+			//swapping the array values
+				array[j+1] = array[j];
+				j = j - 1; 
+			}
+			array[j+1] = key; //Placing the key by 1 increment
+		}
+	}
 }
